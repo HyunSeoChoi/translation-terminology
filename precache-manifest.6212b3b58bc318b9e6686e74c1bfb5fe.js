94e3b71d1ef68acaaada83696447ef55e6212e26
HyunSeoChoi <arenaofjagal@gmail.com>
2020-03-21 13:42:14 +0900

    Updates

---
+++ precache-manifest.6212b3b58bc318b9e6686e74c1bfb5fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9288706e6804b534e1cd545224ae9d1",
+    "url": "/translation-terminology/site/index.html"
+  },
+  {
+    "revision": "a358e5df80a440b8dcb8",
+    "url": "/translation-terminology/site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0085315f9b55f976d5f",
+    "url": "/translation-terminology/site/static/js/2.e6b9613f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/translation-terminology/site/static/js/2.e6b9613f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a358e5df80a440b8dcb8",
+    "url": "/translation-terminology/site/static/js/main.b079961a.chunk.js"
+  },
+  {
+    "revision": "4e6d6885b7ed19405260",
+    "url": "/translation-terminology/site/static/js/runtime-main.06120e36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/translation-terminology/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/translation-terminology/precache-manifest.879d817033d98f00f735889445b1ae38.js"
+  "/translation-terminology/site/precache-manifest.6212b3b58bc318b9e6686e74c1bfb5fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/translation-terminology/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/translation-terminology/site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
