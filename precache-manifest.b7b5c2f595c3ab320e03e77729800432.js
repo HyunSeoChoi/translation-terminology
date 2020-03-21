1fcc4f2846dc118bc5c88df4328914d1f5635f53
HyunSeoChoi <arenaofjagal@gmail.com>
2020-03-21 13:25:07 +0900

    Updates

---
+++ precache-manifest.b7b5c2f595c3ab320e03e77729800432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac15545c52e622ca9832cfba9b8e1af6",
+    "url": "/HyunSeoChoi/translation-terminology/index.html"
+  },
+  {
+    "revision": "04d4d7474378183d3081",
+    "url": "/HyunSeoChoi/translation-terminology/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0085315f9b55f976d5f",
+    "url": "/HyunSeoChoi/translation-terminology/static/js/2.e6b9613f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HyunSeoChoi/translation-terminology/static/js/2.e6b9613f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d4d7474378183d3081",
+    "url": "/HyunSeoChoi/translation-terminology/static/js/main.fa055b5a.chunk.js"
+  },
+  {
+    "revision": "b909ee95258c8772ce0b",
+    "url": "/HyunSeoChoi/translation-terminology/static/js/runtime-main.1dcf2ab2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HyunSeoChoi/translation-terminology/static/media/logo.5d5d9eef.svg"
+  }
+]);
